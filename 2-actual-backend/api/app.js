b285f7d875a1a323123aaf146181a2d9afa6d542
Tanish Thathera <tanishthathera@gmail.com>
2025-08-01 17:39:56 +0530

    Fix localhost and vercel  payment gatway work correctly and also fixing COROS issue 6

--- 2-actual-backend/api/app.js
+++ 2-actual-backend/api/app.js
@@ -11,16 +11,16 @@ const allowedOrigins = ["http://localhost:5173", "https://myntra-clone--frontend
 
  app.use(
    cors({
-    // origin: function(origin, callback) {
-    //   console.log("CORS Origin:", origin);
-    //   if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-    //     callback(null, true);
-    //   } else {
-    //     console.error("CORS Rejected Origin:", origin);
-    //     callback(new Error("Not allowed by CORS"));
-    //   }
-    // },
-     origin: true, // Temporarily allow all origins for debugging CORS issues
+    origin: function(origin, callback) {
+      console.log("CORS Origin:", origin);
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        console.error("CORS Rejected Origin:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    // origin: true,  Temporarily allow all origins for debugging CORS issues
      methods: ["GET", "POST", "DELETE", "OPTIONS"], // ✅ OPTIONS request bhi allow karein
      credentials: true,
      allowedHeaders: ["Content-Type", "Authorization"],
