193ab5ba90f9b8b4b8427a514016c04bdd3f1099
Tanish Thathera <tanishthathera@gmail.com>
2026-03-17 15:02:44 +0530

    Updated product images 2

--- 3-myntera-react-clone/src/components/Sidebar.jsx
+++ 3-myntera-react-clone/src/components/Sidebar.jsx
@@ -9,12 +9,12 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
         <MdClose />
       </button>
       <nav className="side-nav">
-        <Link to="/category/Men" onClick={toggleSidebar}>Men</Link>
-        <Link to="/category/Women" onClick={toggleSidebar}>Women</Link>
-        <Link to="/category/Kids" onClick={toggleSidebar}>Kids</Link>
-        <Link to="/category/Home & Living" onClick={toggleSidebar}>Home & Living</Link>
-        <Link to="/category/Beauty" onClick={toggleSidebar}>Beauty</Link>
-        <Link to="#" onClick={toggleSidebar}>
+        <Link to="/category/men" onClick={toggleSidebar}>Men</Link>
+        <Link to="/category/women" onClick={toggleSidebar}>Women</Link>
+        <Link to="/category/kids" onClick={toggleSidebar}>Kids</Link>
+        <Link to="/category/home-living" onClick={toggleSidebar}>Home & Living</Link>
+        <Link to="/category/beauty" onClick={toggleSidebar}>Beauty</Link>
+        <Link to="/category/studio" onClick={toggleSidebar}>
           Studio <sup>New</sup>
         </Link>
       </nav>
